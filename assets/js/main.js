60933e59bb00f5ae3ab1bacb6279490c4f27bedd
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2026-04-25 13:44:37 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// slider js
+ var $carousel = $('[data-owl-carousel]');
+ if ($carousel.length) {
+     $carousel.each(function (index, el) {
+         $(this).owlCarousel($(this).data('owl-carousel'));
+     });
+ }
